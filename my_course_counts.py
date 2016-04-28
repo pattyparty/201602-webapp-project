13dbd66f7f9588667b13473681b319226180f34f
ellenshin <ellenshin1001@gmail.com>
2016-04-27 21:31:38 -0700

    Edited get_coursecounts, completed the classes

--- my_course_counts.py
+++ my_course_counts.py
@@ -33,17 +33,17 @@ class TimeFrame:
 
 
 class Course:
-    def __init__(self, year, season, department, number, section, time):
+    def __init__(self, year, season, department, number, section):
         self.year = year
         self.season = season
         self.department = department
         self.number = number
         self.section = section
-        self.time = time
         self.title = ''
         self.unit = ''
-        self.instructor = ''
-        self.core = ''
+        self.instructor = []
+        self.meeting = []
+        self.core = []
         self.seats = ''
         self.enrolled = ''
         self.reserved = ''
@@ -51,12 +51,39 @@ class Course:
         self.waitlisted = ''
 
 class Course_Counts:
+    def __init__(self):
+        self.Class = []
+    def search_by_semester(self, sem):
+        pass
+    def search_by_core(self, core):
+        pass
+    def search_by_subject(self, sub):
+        pass
+
+
+def meeting_divide(str):
+    list = str.split(' ')
+    return list
 
 
 def get_course_counts():
-
-
-
+    course_counts = Course_Counts
+    with open('counts.tsv') as fd:
+        for line in fd.read().splitlines():
+            fields = line.split('\t')
+            course = Course(fields[0], fields[1], fields[2], fields[3], fields[4])
+            course.title = fields[5]
+            course.unit = fields[6]
+            course.instructor = fields[7]
+            course.meetings = meeting_divide(fields[8])
+            course.core = fields[9]
+            course.seats = fields[10]
+            course.enrolled = fields[11]
+            course.reserved = fields[12]
+            course.reservedOpen = fields[13]
+            course.waitlisted = fields[14]
+            course_counts.append(course)
+    return course_counts
 
 @app.route('/')
 def view_root():
