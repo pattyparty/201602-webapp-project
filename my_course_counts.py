7f2f6210aaec1df860c5caae18a5f1e55de49fa4
gzanutta <gzanutta@oxy.edu>
2016-04-27 20:33:24 -0700

    dictionary

--- my_course_counts.py
+++ my_course_counts.py
@@ -6,9 +6,24 @@ from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 # FIXME write your app below
+#course count is talking about the classes from the selected semester
+#in that department
 
+#we will need to creat a list of all the departments' classes during
+    #specific semesters
 
+# also we need to fill the dictionary with the semesters
 
+class Subject:
+    def __init__(self, department, semester):
+        dictionary = {"Fall 2017":"f2017"}
+        self.department = department
+        self.semester = semester
+        if (semester == dictionary.keys):
+            course_code = department+"_"+dictionary.values()
+            print(course_code)
+            return course_code
+    
 
 @app.route('/')
 def view_root():
